61ef0b829a26862a27bdfeb9f3acd3c907f059a8
sJingZ <67867862+sJingZ@users.noreply.github.com>
2021-02-06 15:08:23 +0800

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script> 
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-storage.js"></script> 
+
+var firebaseConfig = {
+apiKey: "AIzaSyDz6sUGDRW4biz9XmA3n-pblmAr0m4pevA",
+authDomain: "mywebapp-c28e1.firebaseapp.com",
+databaseURL: "https://mywebapp-c28e1-default-rtdb.firebaseio.com",
+projectId: "mywebapp-c28e1",
+storageBucket: "mywebapp-c28e1.appspot.com",
+messagingSenderId: "285391498914",
+appId: "1:285391498914:web:5063d701128521ac1171fb",
+measurementId: "G-WRGSDBF73H"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
